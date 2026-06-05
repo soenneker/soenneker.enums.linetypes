de7d0bc2f8347c763a2e6d9685cfc36db8de64f7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:31 -0500

    xml commenting

--- src/Soenneker.Enums.LineTypes/LineType.cs
+++ src/Soenneker.Enums.LineTypes/LineType.cs
@@ -10,10 +10,28 @@ namespace Soenneker.Enums.LineTypes;
 [EnumValue<string>]
 public sealed partial class LineType
 {
+    /// <summary>
+    /// The wireline.
+    /// </summary>
     public static readonly LineType Wireline = new(nameof(Wireline));
+    /// <summary>
+    /// The wireless.
+    /// </summary>
     public static readonly LineType Wireless = new(nameof(Wireless));
+    /// <summary>
+    /// The vo wi fi.
+    /// </summary>
     public static readonly LineType VoWiFi = new(nameof(VoWiFi));
+    /// <summary>
+    /// The vo ip.
+    /// </summary>
     public static readonly LineType VoIP = new(nameof(VoIP));
+    /// <summary>
+    /// The pre paid wireless.
+    /// </summary>
     public static readonly LineType PrePaidWireless = new(nameof(PrePaidWireless));
+    /// <summary>
+    /// The unknown.
+    /// </summary>
     public static readonly LineType Unknown = new(nameof(Unknown));
 }
